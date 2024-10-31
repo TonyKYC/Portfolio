20e54ee9df67c28a2dc8e3f7b16aedcdca7c7f97
TonyKYC <anthony.abramo.pro@gmail.com>
2024-11-01 02:23:46 +0800

    Fix: .env prod

--- app/contact/Contact.jsx
+++ app/contact/Contact.jsx
@@ -21,13 +21,10 @@ const Contact = () => {
 
     emailjs
       .send(
-        import.meta.env.VITE_PROD_EMAILJS_SERVICE_ID ||
-          import.meta.env.VITE_EMAILJS_SERVICE_ID,
-        import.meta.env.VITE_PROD_EMAILJS_TEMPLATE_ID ||
-          import.meta.env.VITE_EMAILJS_TEMPLATE_ID,
+        import.meta.env.VITE_EMAILJS_SERVICE_ID,
+        import.meta.env.VITE_EMAILJS_TEMPLATE_ID,
         formData,
-        import.meta.env.VITE_PROD_EMAILJS_USER_ID ||
-          import.meta.env.VITE_EMAILJS_USER_ID
+        import.meta.env.VITE_EMAILJS_USER_ID
       )
       .then((response) => {
         toast.success("Email sent successfully!");
